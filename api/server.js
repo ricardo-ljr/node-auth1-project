03aabdf94aa5fbcd054f77861cdd2d41d64fe1b0
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-03-23 16:11:46 -0600

    Register and Login Work

--- api/server.js
+++ api/server.js
@@ -1,6 +1,33 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+
+const usersRouter = require("../users/users-router.js");
+const authRouter = require("../auth/router");
+const restricted = require("../auth/restricted-middleware");
 
 const server = express();
+
+const sessionConfig = {
+  name: "Cookie Monster",
+  secret: "Keep it secret, keep it safe",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false, //should be true in production
+    httpOnly: true //true means no access from javascript
+  },
+  resave: false,
+  saveUninitialized: true //GDPR laws require to check with client before setting this to true
+};
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig));
+
+server.use("/api/users", restricted, usersRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
